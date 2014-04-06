369d64f1cb4efd782c39d1b100a3a32ce02ff7f9
Phil <CreaturePhil@users.noreply.github.com>
2014-04-05 23:16:28 -0700

    Update system-operators.js

--- source/system-operators.js
+++ source/system-operators.js
@@ -1,6 +1,6 @@
 exports.SystemOperatorOverRide = function () {
 
-    var SystemOperators = ['creaturephil', 'blakjack', 'Arjunb'];
+    var SystemOperators = ['creaturephil', 'blakjack', 'arjunb'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (SystemOperators.indexOf(this.userid) > -1 && this.authenticated) {
