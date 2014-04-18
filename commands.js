c13a11f8bf7043f2f0bfeb875517b61d4859b547
Phil <birkal@outlook.com>
2014-04-18 01:25:31 -0700

    everything put back

--- commands.js
+++ commands.js
@@ -412,7 +412,6 @@ var commands = exports.commands = {
 	},
 
 	stafflista: function(target, room, user, connection) {
-		var t1 = process.hrtime();
 		var buffer = {
 			admins: [],
 			leaders: [],
@@ -449,10 +448,8 @@ var commands = exports.commands = {
 		buffer.mods = buffer.mods.join(', ');
 		buffer.drivers = buffer.drivers.join(', ');
 		buffer.voices = buffer.voices.join(', ');
-		this.sendReply(process.hrtime(t1));
-		return process.hrtime(t1);
-		//connection.popup('Administrators: \n--------------------\n' + buffer.admins + '\n\nLeaders:\n-------------------- \n' + buffer.leaders + '\n\nModerators:\n-------------------- \n' + buffer.mods + '\n\nDrivers: \n--------------------\n' + buffer.drivers + '\n\nVoices:\n-------------------- \n' + buffer.voices);
-		
+
+		connection.popup('Administrators: \n--------------------\n' + buffer.admins + '\n\nLeaders:\n-------------------- \n' + buffer.leaders + '\n\nModerators:\n-------------------- \n' + buffer.mods + '\n\nDrivers: \n--------------------\n' + buffer.drivers + '\n\nVoices:\n-------------------- \n' + buffer.voices);
 	},
 
 
--- source/custom-commands.js
+++ source/custom-commands.js
@@ -178,7 +178,6 @@ var customCommands = {
 	},
 
 	stafflist: function (target, room, user, connection) {
-		var t1 = process.hrtime();
 	    var buffer = [];
 	    var admins = [];
 	    var leaders = [];
@@ -246,9 +245,8 @@ var customCommands = {
 	    if (voices.length > 0) {
 	        voices = voices.join(', ');
 	    }
-	     this.sendReply(process.hrtime(t1));
-	     return process.hrtime(t1);
-	    //connection.popup('Administrators: \n--------------------\n' + admins + '\n\nLeaders:\n-------------------- \n' + leaders + '\n\nModerators:\n-------------------- \n' + mods + '\n\nDrivers: \n--------------------\n' + drivers + '\n\nVoices:\n-------------------- \n' + voices);
+
+	    connection.popup('Administrators: \n--------------------\n' + admins + '\n\nLeaders:\n-------------------- \n' + leaders + '\n\nModerators:\n-------------------- \n' + mods + '\n\nDrivers: \n--------------------\n' + drivers + '\n\nVoices:\n-------------------- \n' + voices);
 	   
 	},
 
