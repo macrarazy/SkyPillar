65548a0aa5a205bfcfd0f1ca79d86415b8173ab2
Chaarizard <aananth2001@gmail.com>
2014-05-19 20:59:45 +0530

    My TC

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -11,6 +11,13 @@ var trainerCards = {
 	    if (!this.canBroadcast()) return;
 	    this.sendReplyBox('<center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif width="150" length="150"><img src=http://i.imgur.com/afSRAAO.png width="250"><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif img width="150" length="150"></center>');
 	},
+	
+        chaarizard: 'char',
+        charizard: 'char',
+        char: function(target,room,user) {
+        	if (!this.canBroadcast()) return;
+        	this.sendReplyBox('<center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif height=125><img src=http://i.imgur.com/j1hggX6.png height=90><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif height=150></center><br><font size=2><center><font color=red><b>Quote:</b></font> Nobody is perfetc</center></font>');
+        },
 
 	arsh: 'blakjack',
 	arshmalik: 'blakjack',
