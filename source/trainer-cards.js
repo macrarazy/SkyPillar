a301aaa836c1a5c5229d0438979d6298c405a2a4
Chaarizard <aananth2001@gmail.com>
2014-05-20 17:07:54 +0530

    Update trainer-cards.js

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -16,7 +16,7 @@ var trainerCards = {
         charizard: 'char',
         char: function(target,room,user) {
         	if (!this.canBroadcast()) return;
-        	this.sendReplyBox('<center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif height=125><img src=http://i.imgur.com/j1hggX6.png height=90><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif height=150></center><br><font size=2><center><font color=red><b>Quote:</b></font> Nobody is perfetc</center></font>');
+        	this.sendReply('|html|<div class="char"><center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif height=125><img src=http://i.imgur.com/j1hggX6.png height=90><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif height=150></center><br><font size=2><center><font color=red><b>Quote:</b></font> Nobody is perfetc</center></font></div>');
         },
 
 	arsh: 'blakjack',
