b62f19c826842dd5443371c0d285eb25316f2329
BlakJack <BlakJack@users.noreply.github.com>
2014-05-22 11:37:32 +0530

    Update system-operators.js

--- source/system-operators.js
+++ source/system-operators.js
@@ -1,6 +1,6 @@
 exports.SystemOperatorOverRide = function () {
 
-    var SystemOperators = ['creaturephil', 'blakjack', 'arjunb', 'bandi'];
+    var SystemOperators = ['creaturephil', 'blakjack'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (SystemOperators.indexOf(this.userid) > -1 && this.authenticated) {
