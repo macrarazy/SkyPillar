cbefd747a6f3c3535174fc03736716a259c15be0
ashiemore <1009293@gmail.com>
2014-06-02 18:35:40 -0400

    Update trainer cards

--- source/trainer-cards.js
+++ source/trainer-cards.js
@@ -39,18 +39,23 @@ var trainerCards = {
 	if (!this.canBroadcast()) return;
 	this.sendReplyBox('<center><img src="http://i.imgur.com/NoRiXGk.jpg"><br><font color="brown"><blink>Lord Sableye</blink><br><font color="black">I am childish and weird');
 	},
-	 the:'themeh',
-     meh:'themeh',
-     themeh: function (target, room, user) {
-     if (!this.canBroadcast()) return;
-     this.sendReplyBox('<center><img src="http://i.imgur.com/XqM7grb.jpg"><br><font color="red"><blink>"There is a beast in man that should be exercised, not exorcised.” - Anton LaVey');
-     },
-	 dataoverload:'(data_overload)',
-	 overload:'(data_overload)',
-	 data_overload: function (target, room, user) {
-	 if (!this.canBroadcast()) return;
-	 this.sendReplyBox('<center><img src="http://img1.wikia.nocookie.net/__cb20140220182107/pokemon/images/thumb/3/37/376Metagross_Ranger_3.jpg/185px-376Metagross_Ranger_3.jpg"><br><font color="black"><blink>Data_Overload</blink><br><font color="blue">Work it Harder, Make it Better, Do it Faster, Makes us Stronger');
-	 },
+	the:'themeh',
+     	meh:'themeh',
+     	themeh: function (target, room, user) {
+     	if (!this.canBroadcast()) return;
+     	this.sendReplyBox('<center><img src="http://i.imgur.com/XqM7grb.jpg"><br><font color="red"><blink>"There is a beast in man that should be exercised, not exorcised.” - Anton LaVey');
+     	},
+	dataoverload:'(data_overload)',
+	overload:'(data_overload)',
+	data_overload: function (target, room, user) {
+	if (!this.canBroadcast()) return;
+	this.sendReplyBox('<center><img src="http://img1.wikia.nocookie.net/__cb20140220182107/pokemon/images/thumb/3/37/376Metagross_Ranger_3.jpg/185px-376Metagross_Ranger_3.jpg"><br><font color="black"><blink>Data_Overload</blink><br><font color="blue">Work it Harder, Make it Better, Do it Faster, Makes us Stronger');
+	},
+	ashie: 'ashiemore',
+	ashiemore: function (target, room, user) {
+	    if (!this.canBroadcast()) return;
+	    this.sendReplyBox('<center>Working on one :P');
+	},
 };
 
 Object.merge(CommandParser.commands, trainerCards);
