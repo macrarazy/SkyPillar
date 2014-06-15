8aa0a3d322ee72ab6467e78bab0119f0f42da0d6
BlakJack <BlakJack@users.noreply.github.com>
2014-06-15 21:20:21 +0530

    Update custom-commands.js

--- source/custom-commands.js
+++ source/custom-commands.js
@@ -946,6 +946,18 @@ target.toLowerCase().replace(/ /g,'-');
 			return user.send('|popup|You have not set made a transactions.log in the logs folder yet.\n\n ' + e.stack);
 		}
 	},
+	masspm: 'pmall',
+	pmall: function (target, room, user) {
+		if (!this.can('pmall')) return false;
+	    if (!target) return this.sendReply('|raw|/pmall <em>message</em> - Sends a PM to every user in a room.');
+
+	    var pmName = '@KTN Bot';
+
+	    for (var i in Users.users) {
+	        var message = '|pm|' + pmName + '|' + Users.users[i].getIdentity() + '|' + target;
+	        Users.users[i].send(message);
+	    }
+	},
 
 	eating: 'away',
        gaming: 'away',
